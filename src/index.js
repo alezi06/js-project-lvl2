a8bea53f0ff8772644fb6ee7358f4ae3fbd28697
alezi06 <alezi06@mail.ru>
2020-01-23 21:29:54 +0300

    step2

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ export default () => {
   program
     .version(process.env.npm_package_version)
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => console.log(firstConfig, secondConfig))
     .parse(process.argv);
 };
