dfa71d7e3f0850997f4091e9bd6ceaba47f03b58
alezi06 <alezi06@mail.ru>
2020-02-07 12:38:30 +0300

    change diff

--- src/index.js
+++ src/index.js
@@ -1,27 +1,82 @@
 import _ from 'lodash';
 import parse from './parsers';
 
-const getDiff = (obj1, obj2) => {
-  const keys = _.uniq(Object.keys(obj1).concat(Object.keys(obj2)));
+const getDiff = (before, after) => {
+  const keys = _.uniq(Object.keys(before).concat(Object.keys(after)));
 
-  const toStr = (obj, key, d) => `  ${d} ${key}: ${obj[key]}`;
-
-  const result = keys.reduce((acc, key) => {
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        return [...acc, toStr(obj2, key, ' ')];
+  return keys.map((key) => {
+    if (_.has(before, key) && _.has(after, key)) {
+      if (before[key] instanceof Object && after[key] instanceof Object) {
+        return {
+          key,
+          type: 'node',
+          children: getDiff(before[key], after[key]),
+        };
       }
-      return [...acc, toStr(obj2, key, '+'), toStr(obj1, key, '-')];
+      if (before[key] === after[key]) {
+        return {
+          key,
+          type: 'unchanged',
+          from: before[key],
+          to: after[key],
+        };
+      }
+      return {
+        key,
+        type: 'changed',
+        from: before[key],
+        to: after[key],
+      };
     }
-    if (_.has(obj1, key)) {
-      return [...acc, toStr(obj1, key, '-')];
+    if (_.has(before, key)) {
+      return {
+        key,
+        type: 'removed',
+        from: before[key],
+        to: null,
+      };
     }
-    return [...acc, toStr(obj2, key, '+')];
-  }, []);
-
-  return `{\n${result.join('\n')}\n}`;
+    return {
+      key,
+      type: 'added',
+      from: null,
+      to: after[key],
+    };
+  });
 };
 
-export default (pathToFile1, pathToFile2) => (
-  getDiff(parse(pathToFile1), parse(pathToFile2))
-);
+const render = (ast) => {
+  const indent = (depth) => '  '.repeat(depth);
+
+  const iter = (items, depth) => {
+    const stringify = (value) => {
+      if (value instanceof Object) {
+        const result = _.map(value, (v, k) => `${k}: ${v}`).join('\n');
+        return `{\n${indent(depth + 3)}${result}\n${indent(depth + 1)}}`;
+      }
+      return value;
+    };
+
+    const buildStr = {
+      node: (obj) => `${indent(depth)}  ${obj.key}: {\n${iter(obj.children, depth + 2)}\n${indent(depth + 1)}}`,
+      added: (obj) => `${indent(depth)}+ ${obj.key}: ${stringify(obj.to)}`,
+      removed: (obj) => `${indent(depth)}- ${obj.key}: ${stringify(obj.from)}`,
+      changed: (obj) => [
+        `${indent(depth)}- ${obj.key}: ${stringify(obj.from)}`,
+        `${indent(depth)}+ ${obj.key}: ${stringify(obj.to)}`,
+      ],
+      unchanged: (obj) => `${indent(depth)}  ${obj.key}: ${stringify(obj.to)}`,
+    };
+
+    const result = items.map((item) => buildStr[item.type](item));
+
+    return _.flatten(result).join('\n');
+  };
+
+  return `{\n${iter(ast, 1)}\n}`;
+};
+
+export default (pathToFile1, pathToFile2) => {
+  const diff = getDiff(parse(pathToFile1), parse(pathToFile2));
+  return render(diff);
+};
